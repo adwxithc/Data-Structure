6f61a9e9ca355d0018ee921a6ec756faa97e4377
adwxithc <adwaithjanardhanan0@gmail.com>
2024-02-16 16:18:43 +0530

    added factorial code

--- workouts-basics/arrSum.js
+++ workouts-basics/arrSum.js
@@ -1,7 +1,16 @@
-const arr=[1,2,3,4,5,6]
+const arr=[1,2,3,4,5,6,5,9]
+//find number of elements in array with sum 10
 
 function ArraySum(arr){
-   
+    const myset= new Set()
+    let count=0
+   for(let i=0;i<arr.length;i++){
+        if(myset.has(10-arr[i])){
+            count++
+        }
+        myset.add(arr[i])
+   }
+   return count
 }
 
 console.log(ArraySum(arr))
